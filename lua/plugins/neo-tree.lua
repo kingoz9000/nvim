d5e2dc44b469138ce81d031f440ab11945c32233
Mathias Sørensen <mkrs24@student.aau.dk>
2024-10-19 15:04:16 +0200

    Changed bindings for neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,8 +7,7 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree <CR>", {})
-    vim.keymap.set("n", "<C-m>", ":Neotree toggle <CR>", {})
+    vim.keymap.set("n", "<C-n>", ":Neotree toggle <CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 	end,
 }
